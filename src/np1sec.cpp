6c11fa68d54b9832de4a60f1425f498bc7bad4a6
Peter Jankuliak <p.jankuliak@gmail.com>
2016-10-28 17:36:22 +0200

    Replace tabs with four spaces

--- src/np1sec.cpp
+++ src/np1sec.cpp
@@ -43,11 +43,11 @@ extern "C" {
 
 // Callback which is called right before the message is sent.
 static gboolean sending_chat_msg_cb(PurpleAccount *account,
-	char **buffer, int id, void* data)
+    char **buffer, int id, void* data)
 {
-	char *tmp = g_strdup_printf("<br/>(np1sec metadata)%s", *buffer);
-	g_free(*buffer);
-	*buffer = tmp;
+    char *tmp = g_strdup_printf("<br/>(np1sec metadata)%s", *buffer);
+    g_free(*buffer);
+    *buffer = tmp;
     return FALSE;
 }
 
@@ -55,7 +55,7 @@ gboolean np1sec_plugin_load(PurplePlugin* plugin) {
     void *conv_handle = purple_conversations_get_handle();
 
     purple_signal_connect(conv_handle, "sending-chat-msg", plugin,
-	    PURPLE_CALLBACK(sending_chat_msg_cb), NULL);
+        PURPLE_CALLBACK(sending_chat_msg_cb), NULL);
 
     return true;
 }
@@ -67,34 +67,34 @@ gboolean np1sec_plugin_unload(PurplePlugin* plugin) {
 
 static PurplePluginInfo info =
 {
-	PURPLE_PLUGIN_MAGIC,
+    PURPLE_PLUGIN_MAGIC,
 
-	/* Use the 2.0.x API */
-	PURPLE_MAJOR_VERSION,                             /* major version  */
-	PURPLE_MINOR_VERSION,                             /* minor version  */
+    /* Use the 2.0.x API */
+    PURPLE_MAJOR_VERSION,                             /* major version  */
+    PURPLE_MINOR_VERSION,                             /* minor version  */
 
-	PURPLE_PLUGIN_STANDARD,                           /* type           */
-	_(PIDGIN_PLUGIN_TYPE),                            /* ui_requirement */
-	0,                                                /* flags          */
-	NULL,                                             /* dependencies   */
-	PURPLE_PRIORITY_DEFAULT,                          /* priority       */
-	_("gtk-equalitie-np1sec"),                        /* id             */
-	_("(n+1)sec"),                                    /* name           */
-	_("0.1"),                                         /* version        */
-	NULL,                                             /* summary        */
-	NULL,                                             /* description    */
+    PURPLE_PLUGIN_STANDARD,                           /* type           */
+    _(PIDGIN_PLUGIN_TYPE),                            /* ui_requirement */
+    0,                                                /* flags          */
+    NULL,                                             /* dependencies   */
+    PURPLE_PRIORITY_DEFAULT,                          /* priority       */
+    _("gtk-equalitie-np1sec"),                        /* id             */
+    _("(n+1)sec"),                                    /* name           */
+    _("0.1"),                                         /* version        */
+    NULL,                                             /* summary        */
+    NULL,                                             /* description    */
 
-	_("eQualit.ie"),                                  /* author         */
-	_("https://equalit.ie"),                          /* homepage       */
+    _("eQualit.ie"),                                  /* author         */
+    _("https://equalit.ie"),                          /* homepage       */
 
-	np1sec_plugin_load,                               /* load           */
-	np1sec_plugin_unload,                             /* unload         */
-	NULL,                                             /* destroy        */
+    np1sec_plugin_load,                               /* load           */
+    np1sec_plugin_unload,                             /* unload         */
+    NULL,                                             /* destroy        */
 
-	NULL,                                             /* ui_info        */
-	NULL,                                             /* extra_info     */
-	NULL,                                             /* prefs_info     */
-	NULL,                                             /* actions        */
+    NULL,                                             /* ui_info        */
+    NULL,                                             /* extra_info     */
+    NULL,                                             /* prefs_info     */
+    NULL,                                             /* actions        */
 };
 
 
@@ -104,8 +104,8 @@ __init_plugin(PurplePlugin *plugin)
     info.name        = _("(n+1)sec Secure messaging");
     info.summary     = _("Provides private and secure conversations for multi-user chats");
     info.description = _("Preserves the privacy of IM communications "
-			 "by providing encryption, authentication, "
-			 "deniability, and perfect forward secrecy.");
+             "by providing encryption, authentication, "
+             "deniability, and perfect forward secrecy.");
 }
 
 PURPLE_INIT_PLUGIN(np1sec, __init_plugin, info)
