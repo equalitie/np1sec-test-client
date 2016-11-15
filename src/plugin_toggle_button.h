f6edd347adca16a4b78c1d206d98110e76a6fb8f
Peter Jankuliak <p.jankuliak@gmail.com>
2016-11-15 14:59:19 +0100

    The toggle button can no longer enable np1sec back
    Because np1sec doesn't yet implement 'disconnect' messages
    which are needed for the other participants to remove the
    user from a channel

--- src/plugin_toggle_button.h
+++ src/plugin_toggle_button.h
@@ -43,7 +43,7 @@ private:
 private:
     PurpleConversation* _conv;
     PidginConversation* _gtkconv;
-    GtkWidget* _button;
+    GtkWidget* _button = nullptr;
 };
 
 //------------------------------------------------------------------------------
@@ -54,45 +54,40 @@ PluginToggleButton::PluginToggleButton(PurpleConversation* conv)
     : _conv(conv)
     , _gtkconv(PIDGIN_CONVERSATION(conv))
 {
-    _button = gtk_button_new_with_label("Toggle np1sec");
+    //if (! purple_prefs_exists("/np1sec")) {
+    //    purple_prefs_add_none("/np1sec");
+    //}
+
+    //if (! purple_prefs_exists("/np1sec/conversation/")) {
+    //    purple_prefs_add_none("/np1sec/conversation");
+    //}
+
+    //auto s = std::string("/np1sec/conversation/") + _conv->name;
+
+    //if (! purple_prefs_exists(s.c_str())) {
+    //    purple_prefs_add_none(s.c_str());
+    //}
+
+	//if (purple_prefs_get_bool(prefs_str().c_str())) {
+    //    enable();
+    //}
+
+    _button = gtk_button_new_with_label("Go secure");
     gtk_box_pack_start(GTK_BOX(_gtkconv->infopane_hbox), _button, FALSE, FALSE, 0);
 
     gtk_widget_show(_button);
 
     gtk_signal_connect(GTK_OBJECT(_button), "clicked"
             , GTK_SIGNAL_FUNC(on_click), this);
-
-
-    if (! purple_prefs_exists("/np1sec")) {
-        purple_prefs_add_none("/np1sec");
-    }
-
-    if (! purple_prefs_exists("/np1sec/conversation/")) {
-        purple_prefs_add_none("/np1sec/conversation");
-    }
-
-    auto s = std::string("/np1sec/conversation/") + _conv->name;
-
-    if (! purple_prefs_exists(s.c_str())) {
-        purple_prefs_add_none(s.c_str());
-    }
-
-	if (purple_prefs_get_bool(prefs_str().c_str())) {
-        enable();
-    }
 }
 
 inline
 void PluginToggleButton::on_click(GtkWidget*, PluginToggleButton* self)
 {
-    if (self->room) {
-        purple_prefs_set_bool(self->prefs_str().c_str(), false);
-        self->disable();
-    }
-    else {
-        purple_prefs_set_bool(self->prefs_str().c_str(), true);
-        self->enable();
-    }
+    purple_prefs_set_bool(self->prefs_str().c_str(), true);
+    gtk_container_remove(GTK_CONTAINER(self->_gtkconv->infopane_hbox), self->_button);
+    self->_button = nullptr;
+    self->enable();
 }
 
 inline
@@ -115,7 +110,9 @@ void PluginToggleButton::disable()
 inline
 PluginToggleButton::~PluginToggleButton()
 {
-    gtk_container_remove(GTK_CONTAINER(_gtkconv->infopane_hbox), _button);
+    if (_button) {
+        gtk_container_remove(GTK_CONTAINER(_gtkconv->infopane_hbox), _button);
+    }
 }
 
 } // np1sec_plugin namespace
